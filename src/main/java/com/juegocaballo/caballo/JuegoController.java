0a299e46cfd3ea9d73c0c5f6319a132caa04fc9a
Usuario <alonsocinco@gmail.com>
2022-08-02 21:24:46 +0200

    Creado el metodo de mezclar la baraja

--- src/main/java/com/juegocaballo/caballo/JuegoController.java
+++ src/main/java/com/juegocaballo/caballo/JuegoController.java
@@ -4,13 +4,16 @@ import com.juegocaballo.caballo.baraja.Carta;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class JuegoController {
     private static HashMap<Integer, Carta> baraja = new HashMap<Integer, Carta>();
-    private static HashMap<Integer, Carta> barajeada = new HashMap<Integer, Carta>();
+    private static HashMap<Integer, Carta> barajeada = new LinkedHashMap<Integer, Carta>();//Tiene que ser Linked para que no me las ordene por clave
 
 
+    /**
+     * Metodo que crea la baraja de cartas
+     */
     protected static void inicializarBaraja() {
         Carta carta;
 
@@ -22,7 +25,9 @@ public class JuegoController {
     }
 
 
-
+    /**
+     * Metodo que mezcla las cartas de manera aleatoria y las añade a el HashMap barajeada
+     */
     protected static void barajar() {
         for (int i = 0; i < 48; i++) {
             int numeroAleatorio = (int) (Math.random() * 48 + 0);
@@ -34,11 +39,20 @@ public class JuegoController {
                 barajeada.put(numeroAleatorio, carta);
             }
         }
+      /*  // Get all the entries in the map into a list
+        List<Map.Entry<Integer, Carta>> entries = new ArrayList<>(baraja.entrySet());
 
+        // Shuffle the list
+        Collections.shuffle(entries);
+
+        Map<Integer, Carta> barajeada = new LinkedHashMap<>();
+        for (Map.Entry<Integer,Carta> entry : entries) {
+            barajeada.put(entry.getKey(), entry.getValue());
+        }
+*/
         //visualizar baraja barajeada
-        for (int i = 0; i < 48; i++) {
-            Carta carta = barajeada.get(i);
-            System.out.println(carta.toString());
+        for (Map.Entry<Integer, Carta> entry : barajeada.entrySet()) {
+             System.out.println(entry.getKey() + " " + entry.getValue());
         }
 
     }
